50a7908dd5c0a1826c2b094c77caef3fb51e1c8c
Tommaso Allevi <tomallevi@gmail.com>
2021-07-12 11:44:32 +0200

    Lint. Rework #1

--- routes/auth.js
+++ routes/auth.js
@@ -1,53 +1,55 @@
 'use strict'
 
 const credentialDatabase = [
-    { id: 1, username: 'allevo', password: 'qwerty', groups: [] },
-    { id: 2, username: 'admin', password: 'admin', groups: ['admin'] },
-    { id: 3, username: 'viewer', password: 'viewer', groups: ['viewer'] },
+  { id: 1, username: 'allevo', password: 'qwerty', groups: [] },
+  { id: 2, username: 'admin', password: 'admin', groups: ['admin'] },
+  { id: 3, username: 'viewer', password: 'viewer', groups: ['viewer'] }
 ]
 
 module.exports = async function (fastify, opts) {
-    fastify.register(require('fastify-jwt'), {
-        secret: 'supersecret'
-    })
+  fastify.register(require('fastify-jwt'), {
+    secret: 'supersecret'
+  })
 
-    fastify.get('/auth/inspect', async function (req, res) {
-        try {
-            await req.jwtVerify()
-        } catch (err) {
-            res.send(err)
-            return
-        }
+  fastify.get('/auth/inspect', inspectTokenAPI)
+  fastify.post('/auth/login', { schema: loginAPIInputSchema }, loginAPI)
+}
 
-        return req.user
-    })
+async function inspectTokenAPI (req, res) {
+  try {
+    await req.jwtVerify()
+  } catch (err) {
+    res.send(err)
+    return
+  }
 
-    fastify.post('/auth/login', {
-        schema: {
-            body: {
-                type: 'object',
-                required: ['username', 'password'],
-                properties: {
-                    username: { type: 'string' },
-                    password: { type: 'string' },
-                }
-            }
-        }
-    }, async function (req, res) {
-        const { username, password } = req.body
+  return req.user
+}
 
-        const userCredential = credentialDatabase.find(r => r.username === username)
-        if (!userCredential) {
-            throw fastify.httpErrors.unauthorized('invalid credentials')
-        }
-        if (userCredential.password !== password) {
-            throw fastify.httpErrors.unauthorized('invalid credentials')
-        }
+const loginAPIInputSchema = {
+  body: {
+    type: 'object',
+    required: ['username', 'password'],
+    properties: {
+      username: { type: 'string' },
+      password: { type: 'string' }
+    }
+  }
+}
+async function loginAPI (req, res) {
+  const { username, password } = req.body
 
-        const { id, groups } = userCredential
+  const userCredential = credentialDatabase.find(r => r.username === username)
+  if (!userCredential) {
+    throw this.httpErrors.unauthorized('invalid credentials')
+  }
+  if (userCredential.password !== password) {
+    throw this.httpErrors.unauthorized('invalid credentials')
+  }
 
-        const token = fastify.jwt.sign({ subject: username, userId: id, groups })
+  const { id, groups } = userCredential
 
-        return { token }
-    })
+  const token = this.jwt.sign({ subject: username, userId: id, groups })
+
+  return { token }
 }
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -12,7 +12,7 @@ test('auth', async (t) => {
       method: 'POST',
       body: {
         username: 'allevo',
-        password: 'qwerty',
+        password: 'qwerty'
       }
     })
     t.equal(res.statusCode, 200)
@@ -25,13 +25,13 @@ test('auth', async (t) => {
         url: '/auth/inspect',
         method: 'GET',
         headers: {
-          Authorization: 'Bearer ' + token,
+          Authorization: 'Bearer ' + token
         }
       })
       t.equal(res.statusCode, 200)
 
       const { userId, subject, groups, iat, ...others } = JSON.parse(res.payload)
-      
+
       t.equal(userId, 1)
       t.equal(subject, 'allevo')
       t.strictSame(groups, [])
@@ -45,25 +45,25 @@ test('auth', async (t) => {
     { name: 'username is required', requestBody: { password: 'qwerty' }, expectedStatusCode: 400 },
     { name: 'password is required', requestBody: { username: 'allevo' }, expectedStatusCode: 400 },
     { name: 'return 401 on user not found', requestBody: { username: 'unknonw', password: 'qwerty' }, expectedStatusCode: 401 },
-    { name: 'return 401 on wrong pwd', requestBody: { username: 'allevo', password: 'wrong' }, expectedStatusCode: 401 },
+    { name: 'return 401 on wrong pwd', requestBody: { username: 'allevo', password: 'wrong' }, expectedStatusCode: 401 }
   ]
   for (const test of validationTests) {
     t.test(test.name, async t => {
       const res = await app.inject({
         url: '/auth/login',
         method: 'POST',
-        body: test.requestBody,
+        body: test.requestBody
       })
       t.equal(res.statusCode, test.expectedStatusCode, `should return ${test.expectedStatusCode}`, { responseBody: res.payload })
     })
   }
-  
+
   t.test('inpect endoint should return 401 on bad jwt', async t => {
     const res = await app.inject({
       url: '/auth/inspect',
       method: 'GET',
       headers: {
-        Authorization: 'Bearer bad-token',
+        Authorization: 'Bearer bad-token'
       }
     })
     t.equal(res.statusCode, 401, 'should return 401', { responseBody: res.payload })
